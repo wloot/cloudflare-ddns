23d68f173019e631825c3873ecf7eb8cae23cc61
Julian Liu <wlootlxt123@gmail.com>
2020-12-17 02:46:25 +0800

    Don't use venv

--- start-sync.sh
+++ start-sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-python3 -m venv venv
-source ./venv/bin/activate
+#python3 -m venv venv
+#source ./venv/bin/activate
 
-pip3 install requests
+#pip3 install requests
 
 cd $DIR
 python3 cloudflare-ddns.py
